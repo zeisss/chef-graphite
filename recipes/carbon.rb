69c1957fd57e9d4af6e545db8a170e01bb46a95a
Stephan Zeissler <stephan.zeissler@adcloud.com>
2013-02-18 14:00:26 +0100

    Make LOG_UPDATES for carbon_cache configurable

--- recipes/carbon.rb
+++ recipes/carbon.rb
@@ -22,6 +22,7 @@ template "/opt/graphite/conf/carbon.conf" do
     :pickle_receiver_interface  => node["graphite"]["carbon"]["pickle_receiver_interface"],
     :storage_dir                => node["graphite"]["storage_dir"],
     :cache_query_interface      => node["graphite"]["carbon"]["cache_query_interface"]
+    :log_updates                => (node["graphite"]["carbon"]["cache_query_interface"] ? "True" : "False")
   )
   notifies :restart, "service[carbon-cache]"
 end
